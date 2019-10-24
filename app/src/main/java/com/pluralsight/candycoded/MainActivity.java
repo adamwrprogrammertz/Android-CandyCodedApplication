92db1be9300be17e6672240916367e9a6011887d
Adam Reuben <adamreuben66@gmail.com>
2019-10-24 14:26:02 -0700

    opening InfoActivity with an intent

--- app/src/main/java/com/pluralsight/candycoded/MainActivity.java
+++ app/src/main/java/com/pluralsight/candycoded/MainActivity.java
@@ -85,14 +85,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
 
-        if (item.getItemId() == R.id.info){
-            Intent infoIntent = new Intent(this,InfoActivity.class);
-            startActivity(infoIntent);
-        }
+        Intent infoIntent = new Intent(this,InfoActivity.class);
+        startActivity(infoIntent);
 
         return super.onOptionsItemSelected(item);
     }
-    
+
 
 
     private void addCandiesToDatabase(Candy[] candies) {
